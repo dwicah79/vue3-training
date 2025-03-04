ecd6157cda2f1ea7b5458a3a10f293fe6c805906
dwicah79 <dwi2100018405@webmail.uad.ac.id>
2025-03-04 16:51:12 +0700

    add method in  option component

--- src/components/OptionComponent.vue
+++ src/components/OptionComponent.vue
@@ -5,11 +5,17 @@ export default {
       name: 'Dwi Cahyo Nugroho',
     }
   },
+  methods: {
+    showName() {
+      console.log(this.name)
+    },
+  },
 }
 </script>
 
 <template>
   <div>
     <h1>{{ name }}</h1>
+    <button @click="showName">Tampilkan Nama Di Console</button>
   </div>
 </template>
