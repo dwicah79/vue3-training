54c1d2192f3e19753f71cda7d0b8323a5b97997c
dwicah79 <dwi2100018405@webmail.uad.ac.id>
2025-03-04 16:39:46 +0700

    create option component

---
+++ src/components/OptionComponent.vue
@@ -0,0 +1,15 @@
+<script>
+export default {
+  data() {
+    return {
+      name: 'Dwi Cahyo Nugroho',
+    }
+  },
+}
+</script>
+
+<template>
+  <div>
+    <h1>{{ name }}</h1>
+  </div>
+</template>
