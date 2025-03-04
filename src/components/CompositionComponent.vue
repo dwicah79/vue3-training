d0620eb0812bb51bc636c9f1b71329ea1cbdb3cc
dwicah79 <dwi2100018405@webmail.uad.ac.id>
2025-03-04 16:51:00 +0700

    create composition component

---
+++ src/components/CompositionComponent.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <h1>{{ name }}</h1>
+    <button @click="showName">Tampilkan Nama Di Console</button>
+  </div>
+</template>
+
+<script setup>
+const name = 'Dwi Cahyo Nugroho'
+
+const showName = () => {
+  console.log(name)
+}
+</script>
