6e3c364189e231e3150ad1a4fc0a5f4aa5f96a70
dwicah79 <dwi2100018405@webmail.uad.ac.id>
2025-03-04 17:15:38 +0700

    create button increment component

---
+++ src/components/ButtonIncrementComponent.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>Tombol Tambah</div>
+
+  <button @click="increment">+</button>
+  {{ count }}
+  <button @click="decrement">-</button>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      count: 0,
+    }
+  },
+  methods: {
+    increment() {
+      this.count++
+    },
+    decrement() {
+      this.count--
+    },
+  },
+}
+</script>
