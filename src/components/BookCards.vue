0296ef25e891187a41baa759d5f34790449a79c9
dwicah79 <dwi2100018405@webmail.uad.ac.id>
2025-03-09 17:27:57 +0700

    create book card component

---
+++ src/components/BookCards.vue
@@ -0,0 +1,14 @@
+<template>
+  <div
+    class="flex flex-col rounded-lg shadow-lg hover:shadow-2xl transition-shadow duration-300 overflow-hidden bg-white"
+  >
+    <slot name="header" />
+    <div class="p-6">
+      <slot name="content" />
+    </div>
+  </div>
+</template>
+
+<script setup>
+// Script setup untuk BookCards
+</script>
